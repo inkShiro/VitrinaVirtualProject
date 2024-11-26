786edc926527dff7aac04ed8fd97d6fd9009551b
inkShiro <inkShiro02@gmail.com>
2024-11-26 10:02:14 -0500

    update backend 1.1.2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,31 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    // Usar express como backend para Vercel
-    logger: ['log', 'error', 'warn', 'debug'],
-  });
+  const app = await NestFactory.create(AppModule);
 
+  // Usar el puerto proporcionado por el entorno o 4000 por defecto
+  const port = process.env.PORT || 4000;
+  
+  // Establecer un prefijo global para las rutas de la API
   app.setGlobalPrefix('api');
 
-  const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // Configuración de Swagger solo en desarrollo
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Cats example')
+      .setDescription('The cats API description')
+      .setVersion('1.0')
+      .addTag('cats')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
 
+  // Habilitar CORS
   app.enableCors();
 
-  // Vercel no requiere que se especifique el puerto como en un servidor tradicional
-  // Si fuera necesario escuchar en un puerto en un entorno tradicional:
-  // await app.listen(4000);
-  app.use(express.json());
+  // Escuchar en el puerto adecuado
+  await app.listen(port, () => {
+    console.log(`Application is running on: http://localhost:${port}`);
+  });
 }
-
 bootstrap();
